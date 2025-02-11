5b4ef56e2927d3166f69b5eb8ee7cdf27654c73a
ENBA22 <enbasankar03@gmail.com>
2025-02-12 00:11:19 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -6,14 +6,7 @@ const createTodoModel = require("./model/todomodel");
 const passch=require("./model/passchecker")
 
 const app = express()
-app.use(cors(
-    {
-        origin:["https://appfr.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-    
-))
+app.use(cors({ origin: 'https://appfr.vercel.app' }));
 app.use(express.json())
 
 
