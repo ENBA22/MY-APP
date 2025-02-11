d8af818c17a34845ea11a9d0e00f5d56fdce5c0f
ENBA22 <enbasankar03@gmail.com>
2025-02-12 00:39:24 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -117,6 +117,5 @@ app.post("/add",(req,res) =>{
 })
 
 
-app.listen(3001,() =>{
-    console.log("Server listening on port 5000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
